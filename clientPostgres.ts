703b370bea74dfd543ad87c91b6d5c629e112728
ruan <ruanramalho1234@gmail.com>
2024-07-29 00:51:16 -0300

    cria env variables

--- clientPostgres.ts
+++ clientPostgres.ts
@@ -7,11 +7,11 @@ class ClientePG {
 
   constructor() {
     this.client = new Client({
-      user: 'root',
-      password: 'root',
-      host: '192.168.128.3',
-      port: 5432,
-      database: 'lavajato',
+      user: process.env.USER_DB,
+      password: process.env.PASS_DB,
+      host: process.env.HOST_DB,
+      port: Number(process.env.PORT_DB),
+      database: process.env.DATABASE_DB,
     })
   }
 
--- seed.ts
+++ seed.ts
@@ -24,8 +24,3 @@ setInterval(async () => {
   await seed.postuuid4((x: (arg0: number, arg1: { id: string; }) => any) => x(1, { id: uuid4() }));
   await seed.postuuid4((x: (arg0: number, arg1: { id: string; }) => any) => x(1, { id: uuid4() }));
 }, 100)
-
-
-const uuid7U = ['019089c2-f180-744c-a6e3-28aed205df79', '019089c2-c5a0-744c-a6df-ae3b5e74b5a8', '019089c2-7e3c-744c-a6e3-28aed205df79', '019089c2-7e3c-744c-a6e3-28aed205df79', '019089c2-7e3c-744c-a6e3-28aed205df79', '019089c2-7e3c-744c-a6e3-28aed205df79', '019089c2-7e3c-744c-a6e3-28aed205df79', '019089c2-7e3c-744c-a6e3-28aed205df79']
-
-const uuid4U = ['38663d24-8a6e-47a2-a270-0b661e6f3435', '4cea6160-f9d8-4989-8145-2f00ec10626d', 'f4b3b3b4-4b3b-4b3b-4b3b-4b3b4b3b4b3b', 'f4b3b3b4-4b3b-4b3b-4b3b-4b3b4b3b4b3b', 'f4b3b3b4-4b3b-4b3b-4b3b-4b3b4b3b4b3b', 'f4b3b3b4-4b3b-4b3b-4b3b-4b3b4b3b4b3b', 'f4b3b3b4-4b3b-4b3b-4b3b-4b3b4b3b4b3b', 'f4b3b3b4-4b3b-4b3b-4b3b-4b3b4b3b4b3b']
